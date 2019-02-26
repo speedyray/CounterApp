12e42c0f1fd15033432f6e5c891c621cb8088922
Raynold Gyasi <speedyray2ray@gmail.com>
2019-02-26 15:55:30 -0500

    Add conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,16 +12,21 @@ class Counter extends Component {
     //      fontSize: 20,
     //      fontWeight: "bold"
     //  };
+
+
+   
     render() { 
+
       
        
         return (  
             <div className="container">
             <img src={this.state.imgUrl} alt="beauty" />
-            <span style={ { fontSize: 30 }}   className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <span style={ { fontSize: 30 }}  className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-primary btn-lg">Increment</button>
              <ul>
-               {this.state.cities.map(city => <l1 key={city} ><h1>{city}</h1></l1>)}
+               {this.state.cities.length === 0 && "No more cities on earth"}
+              {this.aussieCities()}
             </ul>
             </div>
            ); 
@@ -31,12 +36,19 @@ class Counter extends Component {
         let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
-  }
+        }
 
         formatCount(){
         const { count } = this.state;
         return count === 0 ?  <h1>Zero</h1> :  count;
         }
+
+
+        aussieCities(){
+        if (this.state.cities.length === 0 ) return  <h3>No cities for you, Babe!</h3>  
+         return this.state.cities.map(city => <l1 key={city} ><h1>{city}</h1></l1>);
+       }
+       
 }  
  
 export default Counter;
